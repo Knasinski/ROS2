a419e89ab129df5937988d06fb17878529791f24
Knasinski <aknasinski@appliedmfg.com>
2018-02-28 16:14:54 -0500

    Getting runing under Ubuntu software center with RoboWare Studio

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,12 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/al/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/al/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/al/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/indigo/bin:/home/al/.local/share/umake/bin:/opt/ros/indigo/share:/home/al/Clang:/home/al/.local/share/umake/bin:/home/al/Clang:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/al/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/al/catkin_ws/build"
+export PYTHONPATH="/home/al/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/al/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/al/catkin_ws/src:$ROS_PACKAGE_PATH"
